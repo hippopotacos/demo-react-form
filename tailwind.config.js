3fa6256366dcf628a18bfc0065c09e474de25db1
hippopotacos <hippopotacos@gmail.com>
2021-04-05 23:34:51 +0900

    tailwindcss導入

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {},
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
