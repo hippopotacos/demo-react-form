73d795fab71d24be6c917b53c13229b0b3731a3e
hippopotacos <hippopotacos@gmail.com>
2021-04-18 10:53:13 +0900

    horizontal scroll

--- src/pages/About.js
+++ src/pages/About.js
@@ -33,12 +33,80 @@ const About = props => {
   ];
 
   return(
-    <div>
-      <h1>About</h1>
-      <h2 className={styles.subheading}>I am {props.name}</h2>
-      <PieChart width={730} height={250}>
-        <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={100} fill="#82ca9d" label/>
-      </PieChart>
+    <div className="w-96 mx-auto max-w-full border-l border-r">
+      <div>
+        <h1>About</h1>
+        <h2 className={styles.subheading}>I am {props.name}</h2>
+        <div>
+          <PieChart width={250} height={250}>
+            <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={100} fill="#82ca9d" label/>
+          </PieChart>
+        </div>
+        <div className="mt-8">
+          <ul className="overflow-x-auto whitespace-nowrap">
+            <li className="survey inline-block px-8 py-5 bg-blue-700 rounded-lg">
+              <div>
+                <div>graph</div>
+                <p>title</p>
+                <dl>
+                  <div>
+                    <dt>a</dt>
+                    <dd>60%</dd>
+                  </div>
+                  <div>
+                    <dt>b</dt>
+                    <dd>20%</dd>
+                  </div>
+                  <div>
+                    <dt>c</dt>
+                    <dd>20%</dd>
+                  </div>
+                </dl>
+              </div>
+            </li>
+            <li className="survey inline-block px-8 py-5 bg-blue-700 rounded-lg">
+              <div>
+                <div>graph</div>
+                <p>title</p>
+                <dl>
+                  <div>
+                    <dt>a</dt>
+                    <dd>60%</dd>
+                  </div>
+                  <div>
+                    <dt>b</dt>
+                    <dd>20%</dd>
+                  </div>
+                  <div>
+                    <dt>c</dt>
+                    <dd>20%</dd>
+                  </div>
+                </dl>
+              </div>
+            </li>
+            <li className="survey inline-block px-8 py-5 bg-blue-700 rounded-lg">
+              <div>
+                <div>graph</div>
+                <p>title</p>
+                <dl>
+                  <div>
+                    <dt>a</dt>
+                    <dd>60%</dd>
+                  </div>
+                  <div>
+                    <dt>b</dt>
+                    <dd>20%</dd>
+                  </div>
+                  <div>
+                    <dt>c</dt>
+                    <dd>20%</dd>
+                  </div>
+                </dl>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,11 @@ import styles from './Home.module.css';
 class Home extends React.Component {
   render(){
     return(
-      <div>
-        <h1 className={styles.heading}>TOP page</h1>
-        <h2 className="heading">ここはトップページですね</h2>
+      <div className="flex justify-center w-50">
+        <div>
+          <h1 className={styles.heading}>TOP page</h1>
+          <h2 className="heading">ここはトップページですね</h2>
+        </div>
       </div>
     )
   }
