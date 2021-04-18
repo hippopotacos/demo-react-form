63bca913708e6cd82d510d4cf3954161e4018547
hippopotacos <hippopotacos@gmail.com>
2021-04-18 23:32:16 +0900

    add chart

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,52 +1,33 @@
 import React from 'react';
 import styles from './About.module.css';
-import { PieChart, Pie, Text } from 'recharts'
+// import { PieChart, Pie, Text } from 'recharts'
+import { PieChart } from "react-minimal-pie-chart";
+
+const myData = [
+  { title: "Dogs", value: 50, color: "#f00" },
+  { title: "Cats", value: 30, color: "#0f0" },
+  { title: "Dragons", value: 20, color: "#00f" },
+];
 
 const About = props => {
-  const data = [
-    {
-      index: 0,
-      name: 'データ1',
-      value: 300,
-    },
-    {
-      index: 1,
-      name: 'データ2',
-      value: 200,
-    },
-    {
-      index: 2,
-      name: 'データ3',
-      value: 380,
-    },
-    {
-      index: 3,
-      name: 'データ3',
-      value: 80,
-    }
-  ];
-
-  //円グラフの各領域の色を定義
-  const COLORS = [
-    '#2250A2',
-    '#da50a2',
-  ];
-
   return(
     <div className="w-96 mx-auto max-w-full border-l border-r">
       <div>
         <h1>About</h1>
         <h2 className={styles.subheading}>I am {props.name}</h2>
-        <div>
-          <PieChart width={250} height={250}>
-            <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={100} fill="#82ca9d" label/>
-          </PieChart>
-        </div>
         <div className="mt-8">
           <ul className="overflow-x-auto whitespace-nowrap">
             <li className="survey inline-block px-8 py-5 bg-blue-700 rounded-lg">
               <div>
-                <div>graph</div>
+                <div className="flex justify-center">
+                  <PieChart
+                    // your data
+                    data={myData}
+                    // width and height of the view box
+                    viewBoxSize={[150, 150]}
+                    startAngle={-90}
+                  />
+                </div>
                 <p>title</p>
                 <dl>
                   <div>
@@ -66,7 +47,9 @@ const About = props => {
             </li>
             <li className="survey inline-block px-8 py-5 bg-blue-700 rounded-lg">
               <div>
-                <div>graph</div>
+                <div className="flex justify-center">
+
+                </div>
                 <p>title</p>
                 <dl>
                   <div>
@@ -86,7 +69,9 @@ const About = props => {
             </li>
             <li className="survey inline-block px-8 py-5 bg-blue-700 rounded-lg">
               <div>
-                <div>graph</div>
+                <div className="flex justify-center">
+
+                </div>
                 <p>title</p>
                 <dl>
                   <div>
