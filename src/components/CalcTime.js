1b11e9ebd3440667191520d727ab7c601be87f16
hippopotacos <hippopotacos@gmail.com>
2021-04-29 23:12:16 +0900

    おおまかなrelative timeの計算

--- src/components/CalcTime.js
+++ src/components/CalcTime.js
@@ -3,12 +3,32 @@ import React from 'react';
 const CalcTime = props => {
   const postTime = new Date(props.time);
   const nowTime = new Date();
-  const msecDiff = nowTime.getTime() - postTime.getTime();
-  const dayDiff = Math.floor(msecDiff / 1000 / 60 / 60 / 24);
+  const secondsDiff = (nowTime.getTime() - postTime.getTime()) / 1000;
+
+  const minute = 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const month = day * 30;
+  const year = month * 12;
+
+  let result;
+  if(secondsDiff < minute) {
+    result = Math.round(secondsDiff) + '秒前';
+  }else if(secondsDiff < hour) {
+    result = Math.round(secondsDiff/minute) + '分前';
+  }else if(secondsDiff < day) {
+    result = Math.round(secondsDiff/hour) + '時間前';
+  }else if(secondsDiff < month) {
+    result = Math.round(secondsDiff/day) + '日前';
+  }else if(secondsDiff < year) {
+    result = Math.round(secondsDiff/month) + 'ヶ月前';
+  }else{
+    result = Math.round(secondsDiff/year) + '年前';
+  }
 
   return(
     <div>
-      {dayDiff}日前
+      {result}
     </div>
   );
 }
--- src/data/PastTimeData.js
+++ src/data/PastTimeData.js
@@ -1,7 +1,31 @@
 const PastTimeData = [
   {
     name: 'nameA',
+    time: 'Thu Apr 29 2021 23:06:29 GMT+0900 (日本標準時)'
+  },
+  {
+    name: 'nameA',
+    time: 'Thu Apr 29 2021 23:00:29 GMT+0900 (日本標準時)'
+  },
+  {
+    name: 'nameA',
+    time: 'Thu Apr 29 2021 22:24:29 GMT+0900 (日本標準時)'
+  },
+  {
+    name: 'nameA',
+    time: 'Thu Apr 29 2021 18:24:29 GMT+0900 (日本標準時)'
+  },
+  {
+    name: 'nameA',
     time: 'Sun Apr 23 2021 22:24:29 GMT+0900 (日本標準時)'
+  },
+  {
+    name: 'nameA',
+    time: 'Sat Feb 1 2020 22:24:29 GMT+0900 (日本標準時)'
+  },
+  {
+    name: 'nameA',
+    time: 'Sun Dec 1 2019 22:24:29 GMT+0900 (日本標準時)'
   },
   {
     name: 'nameB',
